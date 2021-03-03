34b54bad3320960f9187701292bc04fc8ccbc0e1
Long <longlt.hn@havaz.vn>
2021-03-03 18:29:10 +0700

    fix console

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,9 @@
 import express from "express";
 import chatbotController from "../controllers/chatbotController"
+import bodyParser from "body-parser";
 
 let router = express.Router();
+
 
 let initWebRoutes = (app) => {
     router.get("/", chatbotController.test)
@@ -9,7 +11,10 @@ let initWebRoutes = (app) => {
     router.post("/webhook", chatbotController.postWebHook)
     router.get('/demo-webview', chatbotController.getWebView)
     router.post('/results', chatbotController.postResult)
-    
+
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     return app.use("/", router)
 }
 
