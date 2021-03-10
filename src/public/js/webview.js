96242e42db4291cd5a606f11a96942b6ef0eb7e7
Long <longlt.hn@havaz.vn>
2021-03-10 14:56:27 +0700

    fix payload

--- src/public/js/webview.js
+++ src/public/js/webview.js
@@ -22,8 +22,8 @@ var padding = { top: 20, right: 40, bottom: 0, left: 0 },
 //http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
 var data = [
     { "label": "Dell LAPTOP", "value": 0, "question": "What CSS property is used for specifying the area between the content and its border?", "degree": 0, "percent": 1 }, // padding
-    { "label": "IMAC PRO", "value": 1, "question": "What CSS property is used for changing the font?", "degree": 0, "percent": 95 }, //font-family
-    { "label": "A1", "value": 2, "question": "What CSS property is used for changing the color of text?", "degree": 0, "percent": 1 },
+    { "label": "IMAC PRO", "value": 1, "question": "What CSS property is used for changing the font?", "degree": 0, "percent": 1 }, //font-family
+    { "label": "A1", "value": 2, "question": "What CSS property is used for changing the color of text?", "degree": 0, "percent": 95 },
     { "label": "A2", "value": 2, "question": "What CSS property is used for changing the color of text?", "degree": 0, "percent": 1 },
     { "label": "A3", "value": 2, "question": "What CSS property is used for changing the color of text?", "degree": 0, "percent": 1 },
     { "label": "A4", "value": 2, "question": "What CSS property is used for changing the color of text?", "degree": 0, "percent": 1 }, //color
